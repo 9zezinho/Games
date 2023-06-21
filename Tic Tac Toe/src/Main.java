2dfd37f5d26afcc0ae42af02a424e05c7892a5b5
zezinho <9littlehoney@gmail.com>
2023-06-21 07:52:53 +0100

    final commit for this game

--- Tic Tac Toe/src/Main.java
+++ Tic Tac Toe/src/Main.java
@@ -1,11 +1,10 @@
 /**
  * @author Suresh
  * @date 18/06/2023
- * @version 1.0
- * @since 18/06/2023
+ * @version 1.1.0
+ * @since 21/06/2023
  */
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -18,17 +17,37 @@ import java.util.Scanner;
  */
 
 public class Main {
-    public static void main(String[] args) {
-        char [][] board = {{' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '}};
 
+    public static void main(String[] args) {
+        boolean playAgain = true;
         System.out.println("Let's play tic-tac-toe");
 
-        printBoard(board);
-        chooseNum(board);
+        do {
+            char [][] board = {{' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '}};
 
+            printBoard(board);
+            chooseNum(board);
+
+            Scanner in = new Scanner(System.in);
+            System.out.println("Play Again? 'Y' for yes "
+                    + "'N' for no.");
+            String x = in.nextLine();
+
+            while(!x.equalsIgnoreCase("N") && !x.equalsIgnoreCase("Y")) {
+                System.out.println("Invalid input! Y or N");
+                x = in.nextLine();
+            }
+
+            if (x.equalsIgnoreCase("N")) {
+                playAgain = false;
+            }
+        } while(playAgain);
+
+        System.out.println("Thanks for playing");
     }
 
     /**
+     *
      * Method to choose the number by the player and
      * the computer
      * @param board is the 2 dimensional array that it
@@ -36,67 +55,73 @@ public class Main {
      */
     public static void chooseNum(char[][] board) {
         int plyr = 0;
-        boolean gameIsNotOver = false;
+        int pc = 0;
+        char winner = ' ';
 
-        while(!gameIsNotOver) {
+        while(true) {
             System.out.println("Choose number between 1-9");
             Scanner in = new Scanner(System.in);
+
+            while(!in.hasNextInt()){
+                System.out.println("Please input a number:");
+                in.next(); //Discard the invalid input
+            }
             plyr = in.nextInt();
+
             while(plyr > 9 || plyr == 0) {
                 System.out.println("Please choose the number between from 1 to 9 only");
                 plyr = in.nextInt();
                 in.nextLine();
             }
-            switch (plyr) {
-                case 1 -> board[0][0] = 'X';
-                case 2 -> board[0][1] = 'X';
-                case 3 -> board[0][2] = 'X';
-                case 4 -> board[1][0] = 'X';
-                case 5 -> board[1][1] = 'X';
-                case 6 -> board[1][2] = 'X';
-                case 7 -> board[2][0] = 'X';
-                case 8 -> board[2][1] = 'X';
-                case 9 -> board[2][2] = 'X';
+
+            int row = (plyr - 1) / 3; //this would just give us either 0,1 or 3
+            int col = (plyr - 1) % 3; //same as the row
+
+            //This checks if that cell is already taken? if it is then just restarts.
+            if (board[row][col] != ' ') {
+                System.out.println("That cell is already taken! Please try again.");
+                continue;
             }
+
+            //based on input by user we put the 'X' in the game.
+            board[row][col] = 'X';
             printBoard(board);
 
+            winner = checkWinner(board);
+            if (winner == 'X'){
+                System.out.println("Player wins");
+                break;
+            }
+            if (isBoardFull(board)) {
+                System.out.println("It's a draw");
+                break;
+            }
+
             Random rnd = new Random();
-            int pc = rnd.nextInt(9) + 1;
+            pc = rnd.nextInt(9) + 1;
             System.out.print("PC chooses the number:" + pc);
             System.out.println();
-            while(pc == plyr) {
+
+            //checks if pc chooses the number same as the plyr choose
+            //and if the number is equal to ' '.
+            while(pc == plyr || board[(pc-1)/3][(pc-1)%3] != ' ') {
                 System.out.println("Opps! that's already taken.");
-                pc = rnd.nextInt(9) + 1;
+                pc = rnd.nextInt(9)+ 1;
                 System.out.print("PC chooses the number:" + pc);
                 System.out.println();
             }
-            switch (pc) {
-                case 1 -> board[0][0] = '0';
-                case 2 -> board[0][1] = '0';
-                case 3 -> board[0][2] = '0';
-                case 4 -> board[1][0] = '0';
-                case 5 -> board[1][1] = '0';
-                case 6 -> board[1][2] = '0';
-                case 7 -> board[2][0] = '0';
-                case 8 -> board[2][1] = '0';
-                case 9 -> board[2][2] = '0';
-            }
+            int pcRow = (pc - 1) / 3;
+            int pcCol = (pc - 1) % 3;
+            board[pcRow][pcCol] = 'O';
             printBoard(board);
-            //if the game is not over it keeps on continue.
-            gameIsNotOver = checkWinner(board) != ' ';
-        }
-        char winner = checkWinner(board);
-        if (winner != ' ') {
-            if (winner == 'X') {
-                System.out.println("Player wins:");
-            }else {
+
+            winner = checkWinner(board);
+            if (winner == 'O') {
                 System.out.println("Computer wins");
+                break;
             }
-        } else {
-            System.out.println("Its a draw.");
         }
     }
-
 
     /**
      *
@@ -106,30 +131,60 @@ public class Main {
      * @return ' ' if there is no winner.
      */
     public static char checkWinner(char[][] board) {
+        boolean isDraw = true;
+
         //check columns
-        for(int i = 0; i < board.length; i++) {
-           if(board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
-                return board[i][0];
-           }
-        }
-        //check rows
         for(int i = 0; i < board.length; i++) {
             if(board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
                 return board[0][i];
             }
         }
+        //check rows
+        for(int i = 0; i < board.length; i++) {
+            if(board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return board[i][0];
+            }
+        }
         //check diagonals
-        if(board[0][1] == board[1][1] && board[1][1] == board[2][2]) {
-            return board[0][1];
+        if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return board[0][0];
         }
         if(board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
             return board[0][2];
+        }
+        //check draws
+        for (char[] chars : board) {
+            for (int j = 0; j < board.length; j++) {
+                if (chars[j] == ' ') {
+                    isDraw = false;
+                    break;
+                }
+            }
+        }
+        if (isDraw){
+            return '#';
         }
         return ' ';
 
     }
 
     /**
+     *
+     * Method to check if the board is full
+     */
+    public static boolean isBoardFull(char[][] board) {
+        for (char[] chars: board) {
+            for(int j = 0; j < board.length; j++) {
+                if (chars[j] == ' '){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     *
      * Method to print the board
      * @param board is the char with [][]
      */
@@ -140,5 +195,4 @@ public class Main {
         System.out.println("-+-+-");
         System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
     }
-
 }
