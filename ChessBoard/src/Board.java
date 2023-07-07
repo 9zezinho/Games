09773e1f76af5f1f0435c2cdca4aadca6b439294
zezinho <9littlehoney@gmail.com>
2023-07-07 03:47:29 +0100

    added the pawn movement ability in the board and properly structured the layout of the chessboard in output

--- ChessBoard/src/Board.java
+++ ChessBoard/src/Board.java
@@ -1,21 +1,21 @@
 public abstract class  Board {
-    protected char space = ' ';
-
     public Board(){
 
     }
 
     /**
+     *
      * Method to display the chess board.
      * @param chessboard is the 8x8 grid board.
      */
-    public void displayBoard(char[][] chessboard) {
+    public void displayBoard(String[][] chessboard) {
 
         for (int i = 0; i < chessboard.length; i++) {
             for (int j = 0; j < chessboard[i].length; j++) {
                 System.out.print(chessboard[i][j] + " | ");
             }
-            System.out.println("\n- + - + - + - + - + - + - + -");
+            System.out.println("\n-------+--------+--------+--------" +
+                    "+--------+--------+--------+--------");
         }
     }
 
@@ -23,5 +23,5 @@ public abstract class  Board {
      * This is the move that this piece of chess can do
      * @param chessboard is the 8x8 board.
      */
-    public abstract void moves(char[][] chessboard);
+    public abstract void moves(String[][] chessboard);
 }
--- ChessBoard/src/Pawns.java
+++ ChessBoard/src/Pawns.java
@@ -1,20 +1,25 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Pawns extends Board {
 
 //    protected char diagonal = ' ';
 //    protected boolean check = true;
-    protected char pawn = 'P';
-    protected int plyr;
+    protected String wPawn = "W-Pawn";
+    protected String bPawn = "B-Pawn";
+    protected int plyrChoose;
+    protected int plyrMove;
     protected int comp;
     @Override
-    public void displayBoard(char[][] chessboard) {
+
+    public void displayBoard(String[][] chessboard) {
 
         for (int i = 0; i < chessboard.length; i++){
-            chessboard[1][i] = pawn;
-            chessboard[6][i] = pawn;
+            chessboard[1][i] = wPawn;
+            chessboard[6][i] = bPawn;
         }
-        System.out.println("=============================");
+        System.out.println("==============================================" +
+                "=========================");
         super.displayBoard(chessboard);
 
     }
@@ -26,17 +31,29 @@ public class Pawns extends Board {
      * @param chessboard is the 8x8 board.
      */
     @Override
-    public void moves(char[][] chessboard) {
+    public void moves(String[][] chessboard) {
         displayBoard(chessboard);
         Scanner in = new Scanner(System.in);
-        System.out.println("Please choose the number:");
-        plyr = in.nextInt();
+        System.out.println("Please choose the number you want to move:");
+        plyrChoose = in.nextInt();
+        in.nextLine(); //consume remaining new line character
 
-        int row = (plyr - 1) / 8;
-        int col = (plyr - 1) % 8;
+        int rowChoose = (plyrChoose - 1) / 8;
+        int colChoose = (plyrChoose - 1) % 8;
 
-        if(chessboard[row][col] == ' '){
-            chessboard[row][col] = pawn;
+        chessboard[rowChoose][colChoose] = " ";
+
+        System.out.println("Please choose the number where you want to move:");
+        plyrMove = in.nextInt();
+        int row = (plyrMove - 1) / 8;
+        int col = (plyrMove - 1) % 8;
+
+        if(Objects.equals(chessboard[row][col], " ")) { //checking if the front space only is empty
+            if (plyrChoose >=9 && plyrChoose <=16){
+                chessboard[row][col] = wPawn;
+            } else if (plyrChoose >= 56 && plyrChoose <= 64) {
+                chessboard[row][col] = bPawn;
+            }
         }
         super.displayBoard(chessboard);
     }
