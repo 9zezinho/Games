63821287b4f369f34e3418f067dd796a41c45869
zezinho <9littlehoney@gmail.com>
2023-07-08 17:19:56 +0100

    pawn piece can move and player is switched after one move and the move cannot exceed the 64 as chessboard is 8x8 grid

--- ChessBoard/src/Board.java
+++ ChessBoard/src/Board.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public abstract class  Board {
+    protected final String [][] chessboard =
+            {{"      ","      ","      ","      ","      ","      ","      ","      "},
+                    {"      ","      ","      ","      ","      ","      ","      ","      "},
+                    {"      ","      ","      ","      ","      ","      ","      ","      "},
+                    {"      ","      ","      ","      ","      ","      ","      ","      "},
+                    {"      ","      ","      ","      ","      ","      ","      ","      "},
+                    {"      ","      ","      ","      ","      ","      ","      ","      "},
+                    {"      ","      ","      ","      ","      ","      ","      ","      "},
+                    {"      ","      ","      ","      ","      ","      ","      ","      "}};
     public Board(){
 
     }
@@ -18,6 +29,59 @@ public abstract class  Board {
                     "+--------+--------+--------+--------");
         }
     }
+
+//    public void chooseNum(String[][] chessboard){
+//        int plyr1Choose = 0;
+//        int plyr1Move = 0;
+//        int plyr2Choose = 0;
+//        int plyr2Move = 0;
+//
+//        Scanner in = new Scanner(System.in);
+//
+//        //forces player to choose their own side.
+//        while(true) {
+//            System.out.println("Plyr1 choose a number to move:");
+//            while(!in.hasNext()) {
+//                System.out.println("Please input the number:");
+//                in.next();//discard the invalid input
+//            }
+//            plyr1Choose = in.nextInt();
+//
+//            if (plyr1Choose >= 49 && plyr1Choose <= 64) {
+//                System.out.println("Plyr1 choose a number where you wan to move:");
+//                plyr1Move = in.nextInt();
+//                Pawns pawns = new Pawns();
+//                pawns.chooseNum(chessboard);
+//                break;
+//            } else {
+//                System.out.println("Please choose the number between 49 and 64 at first to move");
+//            }
+//        }
+//
+//        while(true) {
+//            System.out.println("Plyr1 choose a number to move:");
+//            while(!in.hasNext()) {
+//                System.out.println("Please input the number:");
+//                in.next();//discard the invalid input
+//            }
+//            plyr2Choose = in.nextInt();
+//
+//            if (plyr2Choose >= 9 && plyr2Choose <= 16) {
+//                System.out.println("Plyr1 choose a number where you want to move:");
+//                plyr2Move = in.nextInt();
+//                Pawns pawns = new Pawns();
+//                pawns.chooseNum(chessboard);
+//                break;
+//            } else {
+//                System.out.println("Please choose the number between 49 and 64 at first to move");
+//            }
+//        }
+//
+//
+//
+//
+//
+//    }
 
     /**
      * This is the move that this piece of chess can do
--- ChessBoard/src/Pawns.java
+++ ChessBoard/src/Pawns.java
@@ -1,15 +1,13 @@
-import java.util.Objects;
+
 import java.util.Scanner;
 
 public class Pawns extends Board {
 
-//    protected char diagonal = ' ';
-//    protected boolean check = true;
     protected String wPawn = "W-Pawn";
     protected String bPawn = "B-Pawn";
     protected int plyrChoose;
     protected int plyrMove;
-    protected int comp;
+
     @Override
 
     public void displayBoard(String[][] chessboard) {
@@ -21,7 +19,6 @@ public class Pawns extends Board {
         System.out.println("==============================================" +
                 "=========================");
         super.displayBoard(chessboard);
-
     }
 
     /**
@@ -32,30 +29,91 @@ public class Pawns extends Board {
      */
     @Override
     public void moves(String[][] chessboard) {
+        boolean isWhiteTurn = true; // Flag to track the current player's turn
+        int rowMove;
+        int rowChoose;
+        int colChoose;
+        int colMove;
+
         displayBoard(chessboard);
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please choose the number you want to move:");
-        plyrChoose = in.nextInt();
-        in.nextLine(); //consume remaining new line character
+        do {
+            Scanner in = new Scanner(System.in);
+            do {
+                System.out.println("Please choose the number you want to move:");
+                while(!in.hasNextInt()) {
+                    System.out.println("Error! invalid input");
+                    System.out.println("Please choose the number you want to move:");
+                    in.next();
+                }
+                plyrChoose = in.nextInt();
+                in.nextLine(); // Consume remaining new line character
 
-        int rowChoose = (plyrChoose - 1) / 8;
-        int colChoose = (plyrChoose - 1) % 8;
+                System.out.println("Please choose the number where you want to move:");
+                while (!in.hasNextInt()) {
+                    System.out.println("Error! invalid input");
+                    System.out.println("Please choose the number where you want to move:");
+                    in.next();
+                }
+                plyrMove = in.nextInt();
+                in.nextLine();
+                if (plyrChoose > 64 || plyrMove > 64) {
+                    System.out.println("Error! you number shouldn't exceed 64");
+                }
+            } while (plyrChoose > 64 || plyrMove > 64);
 
-        chessboard[rowChoose][colChoose] = " ";
+            rowChoose = (plyrChoose - 1) / 8;
+            colChoose = (plyrChoose - 1) % 8;
 
-        System.out.println("Please choose the number where you want to move:");
-        plyrMove = in.nextInt();
-        int row = (plyrMove - 1) / 8;
-        int col = (plyrMove - 1) % 8;
+            rowMove = (plyrMove - 1) / 8;
+            colMove = (plyrMove - 1) % 8;
 
-        if(Objects.equals(chessboard[row][col], " ")) { //checking if the front space only is empty
-            if (plyrChoose >=9 && plyrChoose <=16){
-                chessboard[row][col] = wPawn;
-            } else if (plyrChoose >= 56 && plyrChoose <= 64) {
-                chessboard[row][col] = bPawn;
+            String sourcePiece = chessboard[rowChoose][colChoose];
+
+            // Check if the current player can move the chosen piece
+            if ((isWhiteTurn && sourcePiece.equals(wPawn)) || (!isWhiteTurn && sourcePiece.equals(bPawn))) {
+                boolean isValidMove;
+                if (isWhiteTurn) {
+                    isValidMove = isValidMoveForWhite(rowChoose, colChoose, rowMove, colMove);
+                } else {
+                    isValidMove = isValidMoveForBlack(rowChoose, colChoose, rowMove, colMove);
+                }
+
+                if (isValidMove) {
+                    chessboard[rowChoose][colChoose] = "      ";
+                    chessboard[rowMove][colMove] = sourcePiece;
+                    isWhiteTurn = !isWhiteTurn; // Switch turns to the other player
+                } else {
+                    System.out.println("Invalid move for the pawn");
+                }
+            } else {
+                System.out.println("Invalid move! You can only move your pawns");
             }
+
+            super.displayBoard(chessboard);
+        } while (true);
+    }
+    private boolean isValidMoveForWhite(int rowChoose, int colChoose,
+                                        int rowMove, int colMove) {
+        //first white row has the option for the pawn to move two steps.
+        if(rowChoose == 1) {
+            return ((rowMove == rowChoose + 1) || (rowMove == rowChoose + 2))
+                    && colMove == colChoose;
+        }else if (rowChoose > 1) { //only one steps after this.
+            return rowMove == rowChoose + 1 && colMove == colChoose;
+            //change piece if the rowMove moves to the row 7//
         }
-        super.displayBoard(chessboard);
+        return false;
+    }
+    private boolean isValidMoveForBlack(int rowChoose, int colChoose,int rowMove, int colMove) {
+        //first black row has the option for the pawn to move two steps.
+        if(rowChoose == 6) {
+            return ((rowMove == rowChoose - 1) || (rowMove == rowChoose - 2))
+                    && colMove == colChoose;
+        }else if (rowChoose < 6) { //only one steps after this.
+            return rowMove == rowChoose + 1 && colMove == colChoose;
+            //change piece if the rowMove moves to the row 0//
+        }
+        return false;
     }
 
 }
