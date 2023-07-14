bfa40c36961b708a53f150ca4deb92380e32726b
zezinho <9littlehoney@gmail.com>
2023-07-14 03:18:41 +0100

    Fixed the bug where black piece couldn't move to empty spaces

--- ChessBoard/src/Queen.java
+++ ChessBoard/src/Queen.java
@@ -6,10 +6,11 @@
  * @version 1.0
  */
 
-public class Queen extends Piece{
+public class Queen extends Piece {
     public Queen(Board board) {
         super(board);
     }
+
     @Override
     public boolean isValidMoveForWhite(int rowChoose, int colChoose,
                                        int rowMove, int colMove) {
@@ -19,19 +20,19 @@ public class Queen extends Piece{
         int currentCol = colChoose + colDir;
 
         //Move horizontal or vertical or diagonally
-        if(rowChoose == rowMove || colChoose == colMove
-                || Math.abs(rowChoose - rowMove) == Math.abs(colChoose - colMove)){
+        if (rowChoose == rowMove || colChoose == colMove
+                || Math.abs(rowChoose - rowMove) == Math.abs(colChoose - colMove)) {
 
             //Vertical movement
             if (colMove == colChoose) {
-                while(currentRow != rowMove) {
+                while (currentRow != rowMove) {
                     if (!board.chessboard[currentRow][currentCol].equals("      ")) {
                         return false;
                     }
                     currentRow += rowDir;
                 }
             } else if (rowMove == rowChoose) { //Horizontal movement
-                while(currentCol != colMove) {
+                while (currentCol != colMove) {
                     if (!board.chessboard[currentRow][currentCol].equals("      ")) {
                         return false;
                     }
@@ -46,9 +47,10 @@ public class Queen extends Piece{
                     currentCol += colDir;
                 }
             }
-            //Catch other player horizontally or vertically
-            return(!board.chessboard[rowMove][colMove].equals(" ")
-                    && !board.stringArray().contains(board.chessboard[rowMove][colMove]));
+            //Catch other player horizontally or vertically or diagonally
+            return (!board.chessboard[rowMove][colMove].equals(" ")
+                    && !board.stringArray().
+                    contains(board.chessboard[rowMove][colMove]));
         }
         return false;
     }
@@ -61,45 +63,46 @@ public class Queen extends Piece{
         int currentRow = rowDir + rowChoose;
         int currentCol = colDir + colChoose;
 
-        if(rowChoose == rowMove || colChoose == colMove ||
-                Math.abs(rowChoose - rowMove) == Math.abs(colChoose - colMove)){
-            //Vertical movement
-            if(colMove == colChoose) {
-                while(currentRow != rowMove) {
-                    if(!board.chessboard[currentRow][currentCol].
-                            equals("      ")){
+        if (rowChoose == rowMove || colChoose == colMove ||
+                Math.abs(rowChoose - rowMove) == Math.abs(colChoose - colMove)) {
+            // Vertical movement
+            if (colMove == colChoose) {
+                while (currentRow != rowMove) {
+                    if (!board.chessboard[currentRow][colMove].
+                            equals("      ")) {
                         return false;
                     }
                     currentRow += rowDir;
                 }
-
-            } else if (rowChoose == rowMove){ //Horizontal movement
-                while(currentCol != colMove) {
-                    if(!board.chessboard[currentRow][currentCol].
+            } else if (rowChoose == rowMove) { // Horizontal movement
+                while (currentCol != colMove) {
+                    if (!board.chessboard[rowMove][currentCol].
                             equals("      ")) {
                         return false;
                     }
                     currentCol += colDir;
                 }
-            } else { //Diagonal movement
-                while(currentRow != rowMove && currentCol != colMove) {
-                    if(!board.chessboard[currentRow][currentCol].equals("      ")){
+            } else { // Diagonal movement
+                while (currentRow != rowMove && currentCol != colMove) {
+                    if (!board.chessboard[currentRow][currentCol].
+                            equals("      ")) {
                         return false;
                     }
                     currentRow += rowDir;
                     currentCol += colDir;
                 }
             }
-            //Catch other player horizontally or vertically or diagonally
-            if(!board.chessboard[rowMove][colMove].equals(" ")
-                    && board.stringArray().contains(board.chessboard[rowMove][colMove])){
+
+            // Capture opponent's piece when moving diagonally
+            if (!board.chessboard[rowMove][colMove].equals(" ") &&
+                    board.stringArray().
+                            contains(board.chessboard[rowMove][colMove])) {
                 return true;
-            }
-            //when moving one step for black piece
-            if(currentRow == rowMove && currentCol == colMove) {
-                return board.chessboard[currentRow][currentCol].equals("      ");
+            } else { // Move to empty spaces
+                return board.chessboard[rowMove][colMove].equals("      ");
             }
         }
         return false;
+
     }
 }
