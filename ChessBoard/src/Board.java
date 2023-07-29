f7228911ea9615a4da7558117c3652c791b8d38b
zezinho <9littlehoney@gmail.com>
2023-07-29 21:07:34 +0100

    Small changes in description to understand better

--- ChessBoard/src/Board.java
+++ ChessBoard/src/Board.java
@@ -14,7 +14,26 @@ import java.util.List;
  * This class is a main board game where the function of chess game takes place
  * like swapping player after turn and display of the board as well as keeping track
  * after each move. All the chess pieces are implementation or valid move is checked
- * in this class for further movement in the game.
+ * in this class for further movement in the game. And you use the number to move the
+ * pieces in the board
+ *
+ *   1    |    2   |    3   |    4   |    5   |    6   |    7   |    8   |
+ * -------+--------+--------+--------+--------+--------+--------+--------
+ *   9    |   10   |   11   |   12   |    13   |   14  |   15  |   16    |
+ * -------+--------+--------+--------+--------+--------+--------+--------
+ *   17   |   18   |   19   |   20   |   21   |   22   |   23   |   24   |
+ * -------+--------+--------+--------+--------+--------+--------+--------
+ *   25   |   26   |   27   |   28   |   29   |   30   |   31   |   32   |
+ * -------+--------+--------+--------+--------+--------+--------+--------
+ *   33   |   34   |   35   |   36   |   37   |   38   |   39   |   40   |
+ * -------+--------+--------+--------+--------+--------+--------+--------
+ *   41   |   42   |   43   |   44   |   45   |   46   |   47   |   48   |
+ * -------+--------+--------+--------+--------+--------+--------+--------
+ *   49   |   50   |   51   |   52   |   53   |   54   |   55   |   56   |
+ * -------+--------+--------+--------+--------+--------+--------+--------
+ *   57   |   58   |   59   |   60   |   61   |   62   |   63   |   64   |
+ * -------+--------+--------+--------+--------+--------+--------+--------
+ *
  */
 
 public class Board {
